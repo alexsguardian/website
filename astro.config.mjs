5c78295c1d514015b1676d213fc16783898fdf63
Alexander Henderson <alexandzors@gmail.com>
2024-11-24 20:57:56 -0500

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import icon from 'astro-icon';
 import partytown from '@astrojs/partytown';
+import node from '@astrojs/node';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -13,6 +14,9 @@ export default defineConfig({
   site: 'https://alexsguardian.net',
   compressHTML: true,
   output: "server",
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [
     mdx(),
     vue(),
