a362bb24ff32659bb36df352404d35b7758ef733
Alexander Henderson <alexandzors@gmail.com>
2025-01-08 01:54:04 -0500

    updated site nav

--- src/navigation.ts
+++ src/navigation.ts
@@ -44,7 +44,7 @@ export const headerData = {
         },
         {
           text: "Alex's Blog",
-          icon: "mdi:external",
+          external: true,
           href: "https://blog.alexsguardian.net",
         },
         {
@@ -59,20 +59,26 @@ export const headerData = {
 export const footerData = {
   links: [
     {
+      title: 'External',
+      links: [
+        { text: `Alex's Blog`, href: 'https://blog.alexsguardian.net', external: true, },
+        { text: 'WISP Portal', href: 'https://uisp.alexsguardian.net/crm', external: true, }
+      ]
+    },
+    {
       title: 'Support',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        { text: 'Affiliate Links', href: '/support#affliate' },
+        { text: 'Discord Server', href: 'https://discord.com', external: true, },
+        { text: 'Loaner Program', href: '/services#loaner' },
+        { text: 'Status', href: 'https://status.alexsguardian.net', external: true, },
       ],
     },
     {
       title: 'Company',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Shop', href: '#' }
+        { text: 'About', href: '/about' },
+        { text: 'Contact', href: '/contact' },
       ],
     },
   ],
@@ -87,6 +93,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="/src/assets/favicons/favicon.svg" alt="alex's guardian logo" loading="lazy"></img>
-    © 2024 - Alex's Guardian. All rights reserved.
+    © 2025 - Alex's Guardian. All rights reserved.
   `,
 };
