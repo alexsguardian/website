09635494cb6f1809e074115a02d754862376b244
Alexander Henderson <25626115+alexandzors@users.noreply.github.com>
2025-11-13 16:14:21 -0500

    init x2

---
+++ TRADEMARKS.md
@@ -0,0 +1,15 @@
+# Trademark and Branding Guidelines
+
+The MIT license for this project applies to the source code, and the original theme's branding, and does not grant any rights to use the trademarks, logos, or other branding of Alex's Guardian, or other company's branding found within this repository.
+
+## Logos
+
+The logos of "Alex's Guardian" are protected by trademark and copyright law. Their use is restricted and requires prior written permission from the respective organizations.
+
+## Other Branding
+
+Other branding materials, including but not limited to, images, documents, and other assets found in this repository are not covered by the MIT license and may not be used without permission.
+
+With the exception of original images, documents, and other assets provided by the theme template. Refer to the theme's license for guidelines on their usage.
+
+For any questions regarding the use of branding materials, please contact Alex's Guardian.
---
+++ astro.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'astro/config'
+import { fileURLToPath } from 'url'
+import compress from 'astro-compress'
+import icon from 'astro-icon'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
+import itsmatteomanfsecurityTxt from '@itsmatteomanf/astro-security-txt'
+
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  site: 'https://alexguardian.net',
+  integrations: [
+    compress(),
+    icon(),
+    mdx(),
+    sitemap(),
+    itsmatteomanfsecurityTxt({
+      contact: 'mailto:webmaster@alexsguardian.net'
+    })
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          logger: {
+            warn: () => {},
+          },
+        },
+      },
+    },
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@public': fileURLToPath(new URL('./public', import.meta.url)),
+      },
+    },
+  },
+})
